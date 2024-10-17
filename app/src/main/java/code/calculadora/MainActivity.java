0f60f025d8666b19567d94b73fb16c17b4d0a4b6
DuvanLT <duvanred0@gmail.com>
2024-10-17 12:35:06 -0500

    diseño

---
+++ app/src/main/java/code/calculadora/MainActivity.java
@@ -0,0 +1,76 @@
+package code.calculadora;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
+
+import org.jetbrains.annotations.TestOnly;
+
+public class MainActivity extends AppCompatActivity {
+    Operar opr = new Operar();
+    private TextView resultado,last;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        resultado = findViewById(R.id.resultado);
+        last = findViewById(R.id.last);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void calcular(View view){
+
+        if(view.getId() != R.id.igual && view.getId() != R.id.parentesis && view.getId() != R.id.eliminar){
+            Button button = (Button) view;
+            String textButton = button.getText().toString();
+            opr.setValor(opr.getValor() + textButton);
+        }
+
+        if(view.getId() == R.id.clear){
+            opr.setValor("");
+            resultado.setText(opr.getValor());
+        }
+
+        if(view.getId() == R.id.eliminar){
+            opr.setValor(opr.getValor().substring(0,opr.getValor().length() - 1));
+            resultado.setText(opr.getValor());
+        }
+
+        if (view.getId() == R.id.parentesis) {
+            opr.setLado_parentesis(opr.lado_parentesis + 1);
+            if(opr.getLado_parentesis()%2 == 0){
+                opr.setValor(opr.getValor() + ")");
+            }else{
+                opr.setValor(opr.getValor() + "*(");
+            }
+
+        }
+        resultado.setText(opr.getValor());
+
+        if(view.getId() == R.id.igual){
+            double operado =  new ExpressionBuilder(opr.getValor()).build().evaluate();
+            String v = String.valueOf(operado);
+            resultado.setText(v);
+            last.setText(opr.getValor() + "=" + v);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/code/calculadora/Operar.java
@@ -0,0 +1,22 @@
+package code.calculadora;
+
+public class Operar {
+    String valor = "";
+    int lado_parentesis = 0;
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public int getLado_parentesis() {
+        return lado_parentesis;
+    }
+
+    public void setLado_parentesis(int lado_parentesis) {
+        this.lado_parentesis = lado_parentesis;
+    }
+
+}
